13918d54756db9add79b07b59084fd582fe4ebd0
Mondo <1armandosx@gmail.com>
2023-11-10 13:43:45 -0500

    Adds alarm

---
+++ src/automation/alarmManager.js
@@ -0,0 +1,120 @@
+// Filename: automation/alarmManager.js
+
+const cron = require('node-cron');
+const {
+  keyboard,
+  Key
+} = require('@nut-tree/nut-js');
+
+// Single array to store the scheduled alarms
+let scheduledAlarms = [];
+
+const parseTimeToEST = (timeString) => {
+  // Parse the time string
+  let [hours, minutes] = timeString.match(/\d+/g).map(Number);
+  const isPM = timeString.toLowerCase().includes('pm');
+
+  if (isPM && hours < 12) hours += 12;
+  if (!isPM && hours === 12) hours = 0;
+
+  // Create a new Date object for today
+  const currentDate = new Date();
+
+  // Set hours and minutes based on the input time
+  currentDate.setHours(hours, minutes, 0, 0);
+
+  // Convert to EST (considering daylight saving time)
+  const estTime = new Date(currentDate.toLocaleString('en-US', {
+    timeZone: 'America/New_York'
+  }));
+
+  return estTime;
+};
+
+
+// Function to check if an alarm time conflicts with existing alarms
+const isAlarmConflict = (alarmTime) => {
+  return scheduledAlarms.some(alarm => alarm.alarmTime.getTime() === alarmTime.getTime());
+};
+
+const executeAlarmSequence = async (youtubeUrl) => {
+  try {
+
+     // Volume control - start by turning volume down completely
+     for (let i = 0; i < 15; i++) {
+      await keyboard.pressKey(Key.AudioVolDown);
+      await keyboard.releaseKey(Key.AudioVolDown);
+    }
+
+    await keyboard.pressKey(Key.LeftSuper, Key.Space);
+    await keyboard.releaseKey(Key.LeftSuper, Key.Space);
+    await keyboard.type("chrome");
+    await keyboard.pressKey(Key.Return);
+    await keyboard.releaseKey(Key.Return);
+
+    // Wait for Chrome to open
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    await keyboard.pressKey(Key.LeftSuper, Key.T);
+    await keyboard.releaseKey(Key.LeftSuper, Key.T);
+    await keyboard.type(youtubeUrl);
+    await keyboard.pressKey(Key.Return);
+    await keyboard.releaseKey(Key.Return);
+
+    // Increment volume every 20 seconds
+    for (let i = 0; i < 16; i++) {
+      await keyboard.pressKey(Key.AudioVolUp);
+      await keyboard.releaseKey(Key.AudioVolUp);
+      await new Promise(resolve => setTimeout(resolve, 20000)); // Wait 20 seconds
+    }
+  } catch (error) {
+    console.error(`Error executing alarm sequence: ${error}`);
+  }
+};
+
+const scheduleAlarm = (alarmTime, youtubeUrl) => {
+  console.log(`Received alarm time: ${alarmTime}`);
+
+  const localAlarmTime = new Date(alarmTime.toLocaleString('en-US', {
+    timeZone: 'America/New_York'
+  }));
+  const cronTime = `${localAlarmTime.getMinutes()} ${localAlarmTime.getHours()} * * *`;
+
+  console.log(`Cron time: ${cronTime}`);
+
+  const job = cron.schedule(cronTime, () => {
+    console.log(`Alarm going off for: ${youtubeUrl}`);
+    executeAlarmSequence(youtubeUrl);
+  }, {
+    scheduled: true,
+    timezone: "America/New_York"
+  });
+
+  scheduledAlarms.push({
+    alarmTime: localAlarmTime,
+    youtubeUrl,
+    cronTime,
+    job
+  });
+
+  console.log(`Alarm scheduled for ${youtubeUrl} at ${localAlarmTime}`);
+};
+
+
+const listAlarms = () => {
+  return scheduledAlarms;
+};
+
+const clearAlarms = () => {
+  scheduledAlarms.forEach(alarm => alarm.job.stop());
+  scheduledAlarms = [];
+  console.log("All alarms cleared");
+};
+
+module.exports = {
+  parseTimeToEST,
+  isAlarmConflict,
+  scheduleAlarm,
+  listAlarms,
+  clearAlarms,
+};
--- src/bot/handlers.js
+++ src/bot/handlers.js
@@ -13,10 +13,17 @@ const {
 } = require('@nut-tree/nut-js');
 
 const aliases = require('../config/aliases.json');
-const { openTerminalAndRun } = require('../automation/shortcuts');
-const { restartSequence } = require('../automation/shortcuts');
+const {
+  openTerminalAndRun
+} = require('../automation/shortcuts');
+const {
+  restartSequence
+} = require('../automation/shortcuts');
 const automationCommands = require('../automation');
-const { exec } = require('child_process');
+const {
+  exec
+} = require('child_process');
+const { parseTimeToEST, isAlarmConflict, scheduleAlarm } = require('../automation/alarmManager');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
@@ -48,9 +55,11 @@ const register = (bot) => {
 
       // Capture the screenshot using macOS's built-in 'screencapture' command
       const screenshotPath = path.join(os.homedir(), 'Desktop', 'sMScreenshots', 'screenshot.png');
-      
+
       if (!fs.existsSync(path.dirname(screenshotPath))) {
-        fs.mkdirSync(path.dirname(screenshotPath), { recursive: true });
+        fs.mkdirSync(path.dirname(screenshotPath), {
+          recursive: true
+        });
       }
 
       exec(`screencapture -x "${screenshotPath}"`, (error, stdout, stderr) => {
@@ -152,32 +161,32 @@ const register = (bot) => {
         return; // Security check
       }
       const command = ctx.message.text.split(' ')[1]; // Get the part after /server
-  
+
       // Resolve the command to its corresponding alias from aliases.json
       const resolvedAlias = aliases[command] || command;
-  
+
       // Logic to decide whether to run npm start (this could be based on a flag, or some other condition)
       const shouldRunNpmStart = true; // For demonstration, set to true
-  
+
       // Form the terminal command
       let terminalCommand = `cd ${resolvedAlias}`;
       if (shouldRunNpmStart) {
         terminalCommand += ' && npm start';
       }
-  
+
       console.log(`Resolved command: ${command}`);
       ctx.reply("Starting " + command); // Send a message back to Telegram
-  
+
       // Invoke nut.js to open terminal and run the command
       await openTerminalAndRun(terminalCommand);
-  
+
       const logBuffer = "Command executed via nut.js";
       ctx.reply(logBuffer); // Send the log buffer back to Telegram
     } catch (error) {
       console.log("An error occurred in /server command handler:", error);
     }
   });
-  
+
   bot.command('pull', async (ctx) => {
     try {
       console.log("Entered /pull command handler.");
@@ -187,22 +196,22 @@ const register = (bot) => {
         return; // Security check
       }
       const repoName = ctx.message.text.split(' ')[1]; // Get the part after /pull
-  
+
       // Resolve the repoName to its corresponding alias from aliases.json
       const resolvedAlias = aliases[repoName] || repoName;
-  
+
       // Logic to decide whether to run git pull (this could be based on a flag, or some other condition)
       const shouldRunGitPull = true; // For demonstration, set to true
-  
+
       // Form the terminal command
       let terminalCommand = `cd ${resolvedAlias}`;
       if (shouldRunGitPull) {
         terminalCommand += ' && git pull';
       }
-  
+
       console.log(`Pulling latest changes for repository: ${repoName}`);
       ctx.reply("Starting git pull for " + repoName); // Send a message back to Telegram
-  
+
       // Invoke nut.js to open terminal and run the command
       await openTerminalAndRun(terminalCommand);
 
@@ -212,7 +221,7 @@ const register = (bot) => {
       await keyboard.type(process.env.PK);
       await keyboard.pressKey(Key.Enter);
       await keyboard.releaseKey(Key.Enter);
-  
+
       const logBuffer = "Git pull executed via nut.js";
       ctx.reply(logBuffer); // Send the log buffer back to Telegram
     } catch (error) {
@@ -230,16 +239,49 @@ const register = (bot) => {
         console.log("Unauthorized access attempt.");
         return; // Security check
       }
-      
+
       // Call the restartSequence function from shortcuts.js
       await restartSequence();
-      
+
       ctx.reply("Server restart sequence initiated."); // Send a confirmation message back to Telegram
     } catch (error) {
       console.log("An error occurred in /restart command handler:", error);
       ctx.reply(`An error occurred: ${error.message}`);
     }
   });
+
+  bot.command('alarm', async (ctx) => {
+    try {
+      console.log("Entered /alarm command handler.");
+      const incomingChatID = `${ctx.message.chat.id}`;
+      if (!AUTHORIZED_CHAT_ID.includes(incomingChatID)) {
+        console.log("Unauthorized access attempt.");
+        return; // Security check
+      }
+  
+      const [command, timeString, youtubeUrl] = ctx.message.text.split(' ');
+      const alarmTime = parseTimeToEST(timeString);  
+  
+      if (isAlarmConflict(alarmTime)) {
+        ctx.reply(`You already have an alarm set for that time.`);
+        return;
+      }  
+  
+      // Use addAlarm function instead of pushing directly to alarms array
+      scheduleAlarm(alarmTime, youtubeUrl);
+  
+      // Respond to the user
+      let replyMessage = `Great! We will be waking you up at ${timeString}! Sleep well!`;
+      if (youtubeUrl) {
+        replyMessage += `\nAlarm video: ${youtubeUrl}`;
+      }
+      ctx.reply(replyMessage);
+    } catch (error) {
+      console.log("An error occurred in /alarm command handler:", error);
+      ctx.reply(`An error occurred: ${error.message}`);
+    }  
+  });
+
 
   bot.on('text', (ctx) => {
     console.log(`Received text: ${ctx.message.text}`);
